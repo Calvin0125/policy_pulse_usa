ac6299180035ea08cbac7e2f901a0b256d322bb3
Calvin Conley <cconley0125@gmail.com>
2024-06-29 18:53:02 -0500

    start legiscan api wrapper

---
+++ lib/legiscan_api.rb
@@ -0,0 +1,62 @@
+class LegiscanApi
+  def initialize
+    @api_key = Rails.application.credentials.legiscan_api_key
+  end
+
+  def get_session_list
+    # Returns an array of sessions
+    # Sample session in array
+    # {
+    #   "session_id" => 2041,
+    #   "state_id" => 52,
+    #   "year_start" => 2023,
+    #   "year_end" => 2024,
+    #   "prefile" => 0,
+    #   "sine_die" => 0,
+    #   "prior" => 0,
+    #   "special" => 0,
+    #   "session_tag" => "Regular Session",
+    #   "session_title" => "2023-2024 Regular Session",
+    #   "session_name" => "118th Congress",
+    #   "dataset_hash" => "6a282aa75396db5e79686190ffabda6d",
+    #   "session_hash" => "6a282aa75396db5e79686190ffabda6d",
+    #   "name" => "118th Congress"
+    # }
+    response = HTTP.get("https://api.legiscan.com/?key=#{@api_key}&op=getSessionList&state=US")
+    JSON.parse(response.body)['sessions']
+  end
+
+  def get_bill_list(session_id)
+    # returns an array of bills
+    # sample bill from array
+    # {
+    #   "bill_id" => 1741372,
+    #   "number" => "HB1",
+    #   "change_hash" => "8253048d2353bc25d532d4be340ddd0a",
+    #   "url" => "https://legiscan.com/US/bill/HB1/2023",
+    #   "status_date" => "2023-03-14",
+    #   "status" => 1,
+    #   "last_action_date" => "2023-03-30",
+    #   "last_action" => "The Clerk was authorized to correct section numbers, punctuation, and cross references, and to make other necessary technical and conforming corrections in the engrossment of H.R. 1.",
+    #   "title" => "Water Quality Certification and Energy Project Improvement Act of 2023 TAPP American Resources Act Transparency, Accountability, Permitting, and Production of American Resources Act",
+    #   "description" => "To lower energy costs by increasing American energy production, exports, infrastructure, and critical minerals processing, by promoting transparency, accountability, permitting, and production of American resources, and by improving water quality certification and energy projects, and for other purposes."
+    # }
+    response = HTTP.get("https://api.legiscan.com/?key=#{@api_key}&op=getMasterList&id=#{session_id}")
+    master_list = JSON.parse(response.body)['masterlist']
+    # First key/value pair in the response is the session, all after that are bills
+    master_list.values[1..-1]
+  end
+
+  def get_bill(bill_id)
+    # returns a bill hash
+    # has many keys, but we are interested in 'status' and 'status_date'
+    response = HTTP.get("https://api.legiscan.com/?key=#{@api_key}&op=getBill&id=#{bill_id}")
+    JSON.parse(response.body)['bill']
+  end
+
+  def get_bill_text(doc_id)
+    response = HTTP.get("https://api.legiscan.com/?key=#{@api_key}&op=getBillText&id=#{doc_id}")
+    # Todo:
+    # How can I decode the full text, which is inside ['text']['doc']
+  end
+end
