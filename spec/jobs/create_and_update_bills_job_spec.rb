90bf026122aacac595522814d8b6087996ce28e5
Calvin Conley <cconley0125@gmail.com>
2024-08-17 17:10:12 -0600

    continued specs

--- spec/jobs/create_and_update_bills_job_spec.rb
+++ spec/jobs/create_and_update_bills_job_spec.rb
@@ -46,17 +46,12 @@ RSpec.describe CreateAndUpdateBillsJob, type: :job do
       [
         { 'bill_id' => 123, 'title' => 'Bill 1', 'status' => 1, 'status_date' => '2024-07-31' },
         { 'bill_id' => 456, 'title' => 'Bill 2', 'status' => 2, 'status_date' => '2024-08-01' },
-        { 'bill_id' => 789, 'title' => 'Bill 3', 'status' => 3, 'status_date' => '2024-08-02' }
-        { 'bill_id' => 135, 'title' => 'Bill 4', 'status' => 4, 'status_date' => '2024-08-03' }
+        { 'bill_id' => 789, 'title' => 'Bill 3', 'status' => 1, 'status_date' => '2024-08-02' }
+        { 'bill_id' => 135, 'title' => 'Bill 4', 'status' => 3, 'status_date' => '2024-08-03' }
       ]
     end
 
-    # Make 123 exist in database and not need to be updated
-    # Check that the code to update it is never reached
     # Mock response for 456 and 789 and 135 bill detail
-    # Make status for 456 not match in database
-    # Make 789 have no summary in database
-    # Make 135 not exist in database
     # Mock bill text response for 456, 789, and 135
     # Make 135 have no text
     # Mock AI summary for 456, 789.
@@ -66,7 +61,21 @@ RSpec.describe CreateAndUpdateBillsJob, type: :job do
     # or could test logging in this test if you want
 
     it 'creates and updates the bills' do
-      bill_1 = 
+      # This bill does not need to be updated because it has a summary and 
+      # the status_last_updated matches the status_date from LegiScan
+      # TODO: check that code to update this bill is never reached
+      bill_1 = create(:bill, legiscan_bill_id: 123, title: 'Bill 1', summary: "This is the summary for bill 1",
+                      status: 1, status_last_updated: DateTime.new(2024, 7 31))
+      # This bill needs to be updated because the status does not match
+      # the new status from LegiScan
+      bill_2 = create(:bill, legiscan_bill_id: 456, title: 'Bill 2', summary: "This is the old summary for bill 2",
+                      status: 3, status_last_updated: DateTime.new(2024, 7, 1))
+      
+      # This bill needs to be updated because it has no summary
+      bill_3 = create(:bill, legiscan_bill_id: 789, title: 'Bill 3', summary: nil,
+                      status: 1, status_last_updated: DateTime.new(2024, 8, 2))
+      
+      # TODO: Check that bill with id 135 is created
     end
   end
 end
