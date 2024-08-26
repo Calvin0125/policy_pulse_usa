f628d24ac74ad06fa8f9dfa45ca1fb865f63b1a2
Calvin Conley <cconley0125@gmail.com>
2024-08-25 19:46:01 -0600

    redirect all paths to root path

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    if params[:path] && params[:path] != '/'
+      redirect_to '/'
+    end
+  end
 end
