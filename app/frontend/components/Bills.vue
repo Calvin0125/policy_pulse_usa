56e08c4dd8d9b24e297375b4ee337fa77de55af6
Calvin Conley <cconley0125@gmail.com>
2024-08-31 15:19:19 -0600

    working home page

--- app/frontend/components/Bills.vue
+++ app/frontend/components/Bills.vue
@@ -1,17 +1,57 @@
 <template>
-  <div>
-    <h1>Hello from Vue!</h1>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="(bill, index) in bills"
+        :key="index"
+        cols="12"
+      >
+        <v-card class="mx-auto" max-width="800">
+          <v-card-title class="bill-title">{{ bill.title }}</v-card-title>
+          <v-card-subtitle class="bill-attribute">Status: {{ capitalize(bill.status) }}</v-card-subtitle>
+          <v-card-subtitle class="bill-attribute">Status Date: {{ bill.status_last_updated }}</v-card-subtitle>
+          <v-card-text class="bill-attribute">
+            {{ bill.summary || "Text not available yet." }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'Bills'
+  data() {
+    return {
+      bills: []
+    };
+  },
+  beforeMount() {
+    this.fetchBills();
+  },
+  methods: {
+    async fetchBills() {
+      const response = await axios.get('/bills', { headers: { 'Accept': 'application/json' }, params: { page: 1 } });
+      this.bills = response.data.bills;
+    },
+    capitalize(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    }
+  }
 }
 </script>
 
 <style scoped>
-h1 {
-  color: #42b983;
+.bill-title {
+  font-size: 1.5rem;
+  font-weight: bold;
+  white-space: normal;
+  overflow: visible;
+}
+
+.bill-attribute {
+  font-size: 1rem;
 }
 </style>
--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,7 +1,17 @@
 import { createApp } from 'vue'
 import Bills from '../components/Bills.vue'
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 document.addEventListener('DOMContentLoaded', () => {
-  const app = createApp(Bills)
-  app.mount('#app')
-})
+  const app = createApp(Bills);
+  app.use(vuetify);
+  app.mount('#app');
+});
